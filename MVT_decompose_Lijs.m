2bf7483dbc6173a862cf4b325c7dd38e151faf60
Andrew Walker <andrew.walker@bristol.ac.uk>
2013-07-11 20:42:46 +0100

    Put rotation vector on principal axis frame
    And fix documentation

--- MVT_decompose_Lijs.m
+++ MVT_decompose_Lijs.m
@@ -1,10 +1,41 @@
 % MVT_decompose_Lijs - Extract strain rate and vorticity from veloc grad
 %
-% Given a list of velocity gradient tensors, an (3, 3, n) matrix, return 
-% some useful numbers describing the defomation. 
+% Return the strain rate, rotation rate, and strain rate decomposed onto 
+% the principal strain axis from a general velocity gradient tensor (or a
+% list of tensors). Values returned are:
 %
+%     E and W. Strain rate and rotation rate tensors expressed on the input
+%     frame of reference. For the symmetric strain rate tensor 
+%     E_ij = (L_ij + L_ji) / 2 while for the skew symmetric rotation rate 
+%     tensor W_ij = (L_ij - L_ji) / 2. Note that L = E + W. 
+%
+%     e and w. Magnitudes of the principal strain rates and rotation vector
+%     describing the rotation around the principal strain axes. These are
+%     found by diagonalization of the strain rate tensor:
+%         E^p = R^{-1} * E * R ,
+%     by finding the Eigenvalues (the diagonal components of E^p) and 
+%     Eigenvectors (columns of R) of E. These are sorted such that e(1)
+%     = E^p(1,1) is the largest (most positive) strain rate, and e(3) is
+%     the smallest (most negative) strain rate. The rotation rate vector
+%     is then found by rotating the velocity gradient tensor onto this 
+%     frame of reference (L^p = R{^-1} * L * R) and extracting values. 
+%     The velocity gradient tensor on the frame of the principal axes can
+%     be reconstructed from:
+%
+%               |   e(1)   -w(3)/2   w(2)/2  | 
+%         L^p = |  w(3)/2    e(2)   -w(1)/2  |
+%               | -w(2)/2   w(1)/2    e(3)   |
+%
+%     and the input L recovered by L = R * L^p * R^{-1}
 % Usage: 
-%     [] = MVT_decompose_Lijs(Lijs)
+%     [E, W, e, w, R] = MVT_decompose_Lijs(Lij)
+%         Given a velocity gradient tensor, a (3, 3) matrix, 
+%         return the details described above describing the defomation.
+%
+%     [Es, Ws, es, ws, R] = MVT_decompose_Lijs(Lijs)
+%         Given a list of velocity gradient tensors, a (3, 3, n) matrix, 
+%         return lists describing the deformation (tensor number is on the
+%         final axis, otherwise as above).
 %
 % See also: MVT_read_Lij_file
 
@@ -43,28 +74,45 @@
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-function [Es, Ws, e, pa, vort] = MVT_decompose_Lijs(Lijs)
+function [Es, Ws, e, w, R] = MVT_decompose_Lijs(Lijs)
 
     s = size(Lijs);
     assert((s(1) == 3), 'First dimension of Lijs must be size 3\n'); 
     assert((s(2) == 3), 'First dimension of Lijs must be size 3\n');
-    n_t = s(3);
-    
-    Es = zeros(s);
-    Ws = zeros(s);
-    e = zeros(3, n_t);
-    pa = zeros(s);
-    vort = zeros(3, n_t);
-    for n=1:n_t
-        [E, W] = decomp(Lijs(:,:,n));
-        Es(:,:,n) = E(:,:);
-        Ws(:,:,n) = W(:,:);
-        vort(1,n) = W(3,2)*2.0;
-        vort(2,n) = W(1,3)*2.0;
-        vort(3,n) = W(1,2)*2.0;
-        [e(:,n) pa(:,:,n)] = principStrain(E); 
+    if length(s) == 3
+        % Work through a list of tensors.
+        n_t = s(3);  
+        Es = zeros(s);
+        Ws = zeros(s);
+        e = zeros(3, n_t);
+        R = zeros(s);
+        w = zeros(3, n_t);
+        for n=1:n_t
+           
+            [Es(:,:,n), Ws(:,:,n), e(:,n), R(:,:,n), w(:,n)] ...
+                = decompose_serial(Lijs(:,:,n));
+        end
+    elseif length(s) == 2
+        % Only one vel grad tensor
+            [Es, Ws, e, R, w] ...
+                = decompose_serial(Lijs(:,:));
+    else
+        error('Argument must be 3x3xn or 3x3')
     end
 
+end
+
+function [E, W, e, pa, vt] = decompose_serial(L)
+    % Extract strain rate and rotation rate on the input ref. frame
+    [E, W] = decomp(L);
+    % Find the magnitude and orientation of the principal strain rate
+    [e pa] = principStrain(E);
+    e = e'; 
+    % Rotate input vel grad tensor to principal axis frame
+    Lr = pa'*L*pa;
+    % On this principal frame the off-diagonals give the rotation around
+    % the principal axes
+    vt = [Lr(3,2)*2.0; Lr(1,3)*2.0; Lr(1,2)*2.0];
 end
 
 function [e, pa] = principStrain(E)
