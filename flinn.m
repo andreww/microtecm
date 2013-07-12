6096dfd1d28dfcf184c9e73d9dd10c72525c4fa1
Andrew Walker <andrew.walker@bristol.ac.uk>
2013-07-12 17:23:33 +0100

    More work on flinn

--- flinn.m
+++ flinn.m
@@ -4,8 +4,8 @@ function flinn(file)
 
     Ls = MVT_read_Lij_file(file);
     [E, W, e, w, R] = MVT_decompose_Lijs(Ls);
-    y=log(1+e(1,:)./1+e(2,:));
-    x=abs(log(1+e(2,:)./1+e(3,:))); % Plot on +ve x axis (e(3) is always -ve)
+    y=log((1+e(1,:))./(1+e(2,:)));
+    x=(log((1+e(2,:))./(1+e(3,:)))); % Plot on +ve x axis (e(3) is always -ve)
     col = 1-abs([w(1,:)./(2*e(1,:)); ...
                  w(2,:)./(2*e(1,:)); ...
                  w(3,:)./(2*e(1,:))])'; % Simple shear is [010]. Hope
@@ -13,14 +13,20 @@ function flinn(file)
                                            % shear Pure shear is white
     scatter(x,y,20,1:101,'filled');
     colorbar
-    hold
-    plot([0 4.0E-3], [0 4.0E-3])
-    axis([0 4.0E-3 0 4.0E-3])
+    %hold
+    %plot([0 4.0E-3], [0 4.0E-3])
+    %axis([0 4.0E-3 0 4.0E-3])
     pause
     figure
     scatter3(col(:,1), col(:,2), col(:,3), 20, 1:101, 'filled')
     pause
+    figure
     rgbplot(col);
+    pause
+    figure
+    y = abs(col(:,2));
+    x = abs(col(:,1))-abs(col(:,3));
+    scatter(x, y, 20, 1:101, 'filled')
 end
                                            
                                            
